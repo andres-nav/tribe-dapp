19fae8b98cc8891a8e9811a0cb6f6a0f5a7d9faf
Andres Navarro <git@andresnav.com>
2023-06-21 00:40:02 +0200

    (dev) added custom wallets

--- dapp/src/pages/_app.tsx
+++ dapp/src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
+
+import {
+  RainbowKitProvider,
+  connectorsForWallets,
+} from "@rainbow-me/rainbowkit";
+import {
+  injectedWallet,
+  metaMaskWallet,
+  ledgerWallet,
+  walletConnectWallet,
+  coinbaseWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { mainnet, sepolia, hardhat } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
@@ -10,6 +21,7 @@ import "../styles/globals.scss";
 import "@rainbow-me/rainbowkit/styles.css";
 
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+const appName = process.env.NEXT_PUBLIC_APP_NAME;
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
@@ -21,11 +33,21 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
   [alchemyProvider({ apiKey: process.env.ALCHEMY_API }), publicProvider()]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: "Tribe dApp",
-  projectId: "41d7861201cc2f2970bc2bc808a03890",
-  chains,
-});
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      injectedWallet({ chains }),
+      metaMaskWallet({ projectId, chains }),
+      ledgerWallet({ projectId, chains }),
+      walletConnectWallet({ projectId, chains }),
+    ],
+  },
+  {
+    groupName: "Others",
+    wallets: [coinbaseWallet({ chains, appName })],
+  },
+]);
 
 const wagmiConfig = createConfig({
   autoConnect: true,
