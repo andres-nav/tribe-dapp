18833f40e308caf393152429f914820cc129346c
Andres Navarro <git@andresnav.com>
2023-06-22 23:56:22 +0200

    (config) to publish to sepolia

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,7 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-require("./tasks/faucet");
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY;
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -12,6 +18,15 @@ const config: HardhatUserConfig = {
         runs: 200,
       },
     },
+  },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_KEY,
   },
 };
 
