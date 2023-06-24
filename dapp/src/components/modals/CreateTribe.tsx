c9eb26c29bb6d4fe19aa8a487da06c4f36c40e22
Andres Navarro <git@andresnav.com>
2023-06-24 18:19:40 +0200

    (dev) created create tribe form

---
+++ dapp/src/components/modals/CreateTribe.tsx
@@ -0,0 +1,75 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+export default function ModalCreateTribe(props) {
+  const createTribe = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
+  return (
+    <Modal {...props} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Create new tribe</Modal.Title>
+      </Modal.Header>
+
+      <Form onSubmit={createTribe}>
+        <Modal.Body>
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="basic-addon1">Name</InputGroup.Text>
+            <Form.Control
+              required
+              type="text"
+              placeholder="My awesome tribe!"
+              aria-label="My awesome tribe!"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+          <InputGroup className="mb-3">
+            {}{" "}
+            <InputGroup.Text id="basic-addon1">Price to join</InputGroup.Text>
+            <Form.Control
+              required
+              type="number"
+              min="0"
+              step="0.00000000001"
+              placeholder="0.01"
+              aria-label="0.01"
+              aria-describedby="basic-addon1"
+            />
+            <InputGroup.Text id="basic-addon1">ETH</InputGroup.Text>
+          </InputGroup>
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="basic-addon1">Max capacity</InputGroup.Text>
+            <Form.Control
+              required
+              type="number"
+              min="0"
+              placeholder="100"
+              aria-label="100"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="basic-addon1">Link</InputGroup.Text>
+            <Form.Control
+              required
+              type="text"
+              placeholder="www.myawesometribe.com"
+              aria-label="www.myawesometribe.com"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Form>
+    </Modal>
+  );
+}
--- dapp/src/pages/index.tsx
+++ dapp/src/pages/index.tsx
@@ -1,23 +1,34 @@
+import { useState } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Button } from "react-bootstrap";
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
 
-const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Tribe dApp</title>
-        <meta content="Tribe dApp: join your tribe!" name="description" />
-        <link href="/favicon.ico" rel="icon" />
-      </Head>
+import ModalCreateTribe from "../components/modals/CreateTribe";
 
-      <div className={styles.floating}>
-        <ConnectButton />
+const Home: NextPage = () => {
+  const [modalShow, setModalShow] = useState(false);
+
+  return (
+    <>
+      <div className={styles.container}>
+        <Head>
+          <title>Tribe Dapp</title>
+          <meta content="Tribe Dapp: join your tribe!" name="description" />
+          <link href="/favicon.ico" rel="icon" />
+        </Head>
+
+        <div className={styles.floating}>
+          <ConnectButton />
+        </div>
+        <main className={styles.main}>
+          <h1> main</h1>
+          <Button onClick={() => setModalShow(true)}>create Tribe</Button>
+        </main>
       </div>
-      <main className={styles.main}></main>
-    </div>
+      <ModalCreateTribe show={modalShow} onHide={() => setModalShow(false)} />
+    </>
   );
 };
 
